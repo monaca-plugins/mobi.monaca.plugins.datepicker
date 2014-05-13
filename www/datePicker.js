a9791638733eda3ff526c31cc6e5ca68f1c2e6ba
shikatah <shikatah@asial.co.jp>
2014-05-13 17:27:37 +0900

    fix plugin's js.

--- www/datePicker.js
+++ www/datePicker.js
@@ -3,59 +3,51 @@
     Copyright (c) Greg Allen 2011
     MIT Licensed
 **/
-window.plugins = window.plugins || {}
+var DatePicker = function(){
+    this._callback;
+}
 
-if (!window.plugins.datePicker) {
-    /* shim to work in 1.5 and 1.6 */
-    if (!window.Cordova) {
-        window.Cordova = cordova;
+/**
+* show - true to show the ad, false to hide the ad
+*/
+DatePicker.prototype.show = function(options, cb) {
+
+    var padDate = function(date) {
+      if (date.length == 1) {
+        return ("0" + date);
+      }
+      return date;
     };
 
-    /**
-     * Constructor
-     */
-    function DatePicker() {
-        this._callback;
+    if (options.date) {
+        options.date = options.date.getFullYear() + "-" +
+                       padDate(options.date.getMonth()+1) + "-" +
+                       padDate(options.date.getDate()) +
+                       "T" + padDate(options.date.getHours()) + ":" +
+                       padDate(options.date.getMinutes()) + ":00Z";
+    }
+    
+    var defaults = {
+        mode: 'datetime',
+        date: '',
+        allowOldDates: true,
+        allowFutureDates : true
+    }
+    for (var key in defaults) {
+        if (typeof options[key] !== "undefined")
+            defaults[key] = options[key];
     }
 
-    /**
-     * show - true to show the ad, false to hide the ad
-     */
-    DatePicker.prototype.show = function(options, cb) {
-        var padDate = function(date) {
-          if (date.length == 1) {
-            return ("0" + date);
-          }
-          return date;
-        };
+    this._callback = cb;
+    Cordova.exec(null, null, "DatePicker", "show", [defaults]);
+}
 
-        if (options.date) {
-            options.date = options.date.getFullYear() + "-" +
-                           padDate(options.date.getMonth()+1) + "-" +
-                           padDate(options.date.getDate()) +
-                           "T" + padDate(options.date.getHours()) + ":" +
-                           padDate(options.date.getMinutes()) + ":00Z";
-        }
-        var defaults = {
-            mode: 'datetime',
-            date: '',
-            allowOldDates: true,
-            allowFutureDates : true
-        }
-        for (var key in defaults) {
-            if (typeof options[key] !== "undefined")
-                defaults[key] = options[key];
-        }
-        this._callback = cb;
-        Cordova.exec(null, null, "DatePicker", "show", [defaults]);
+DatePicker.prototype._dateSelected = function(date) {
+    var d = new Date(parseFloat(date)*1000);
+    if (this._callback){
+        this._callback(d);
     }
+        
+}
 
-    DatePicker.prototype._dateSelected = function(date) {
-        var d = new Date(parseFloat(date)*1000);
-        if (this._callback)
-            this._callback(d);
-    }
-
-
-    module.exports = new DatePicker();
-};
+module.exports = new DatePicker();
